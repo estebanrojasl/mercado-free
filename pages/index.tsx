8489cd2ee9c69609f92881e72ee1ae442196e96d
Esteban Rojas Londoño <esteban7590@hotmail.com>
2021-07-03 12:46:24 -0500

    Added database connection and model

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,33 +2,10 @@ import { GetStaticProps } from "next";
 import { CardProps } from "../components/Card/Card";
 import Products from "../components/Products/Products";
 
+import prisma from "../lib/prisma";
+
 export const getStaticProps: GetStaticProps = async () => {
-  const data = [
-    {
-      id: 1,
-      title: "Gafas PitViper",
-      url: "https://articulo.mercadolibre.com.co/MCO-519442671-gafas-pixeladas-thug-life-divertidas-turn-down-vintage-ok-_JM?searchVariation=83702945720#searchVariation=83702945720&position=2&search_layout=grid&type=item&tracking_id=6ad39465-7a13-40e3-b364-0a57907df055",
-      price: "400000",
-      mprice: "700000",
-      image:
-        "https://cdn.shopify.com/s/files/1/0185/7770/t/94/assets/806bcaf86561--blacking-out-spit_x350.png?v=1590526590",
-      author: "therock",
-      avatar:
-        "https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg",
-    },
-    {
-      id: 2,
-      title: "Gafas PitViper",
-      url: "https://articulo.mercadolibre.com.co/MCO-519442671-gafas-pixeladas-thug-life-divertidas-turn-down-vintage-ok-_JM?searchVariation=83702945720#searchVariation=83702945720&position=2&search_layout=grid&type=item&tracking_id=6ad39465-7a13-40e3-b364-0a57907df055",
-      price: "400000",
-      mprice: "700000",
-      image:
-        "https://cdn.shopify.com/s/files/1/0185/7770/t/94/assets/806bcaf86561--blacking-out-spit_x350.png?v=1590526590",
-      author: "therock",
-      avatar:
-        "https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg",
-    },
-  ];
+  const data = await prisma.product.findMany({});
   return { props: { data } };
 };
 
