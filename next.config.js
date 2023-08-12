91812ed5b826b74e27ca3d3f2d1325fd85b16524
Esteban Rojas Londoño <esteban7590@hotmail.com>
2023-08-12 11:31:27 -0500

    .

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ module.exports = {
   images: {
     domains: ["http2.mlstatic.com"],
   },
+  experimental: {
+    outputFileTracingExcludes: [
+      "**caniuse-lite**",
+      "**styled-jsx**",
+      "**react-dom**",
+      "**next**",
+    ],
+  },
 };
