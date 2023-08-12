c31a490e3d2db0f681352e3036c0aaa8a6dac86c
Esteban Rojas Londoño <esteban7590@hotmail.com>
2023-08-12 10:20:54 -0500

    reduce scrape size

--- pages/api/scrape.ts
+++ pages/api/scrape.ts
@@ -1,55 +1,53 @@
 // POST /api/post
 import type { NextApiRequest, NextApiResponse } from "next";
-import puppeteer from "puppeteer";
+import puppeteer, { Browser } from "puppeteer";
 import chromium from "chrome-aws-lambda";
+
+interface MercadoInfo {
+  title: string | null;
+  image: string;
+  price: string | null;
+}
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<MercadoInfo | string>
 ) {
   const { url } = req.body;
 
   try {
-    const browser = await puppeteer.launch({
+    const browser: Browser = await puppeteer.launch({
       args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath,
       headless: true,
       ignoreHTTPSErrors: true,
     });
-    // const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     const page = await browser.newPage();
     await page.goto(url);
 
     await page.waitForSelector("[class=ui-pdp-title]");
-    const mercadoInfo = await page.evaluate(() => {
-      const title = document.querySelector("h1")?.innerHTML;
+    const mercadoInfo: MercadoInfo = await page.evaluate(() => {
+      const title = document.querySelector("h1")?.textContent ?? null;
       const image = (
         document.querySelector(
           ".ui-pdp-gallery__figure__image"
         ) as HTMLImageElement
       ).src;
-      const price = document
-        .querySelector(
-          ".ui-pdp-price__second-line .andes-money-amount__fraction"
-        )
-        ?.innerHTML.replaceAll(".", "");
+      const price =
+        document
+          .querySelector(
+            ".ui-pdp-price__second-line .andes-money-amount__fraction"
+          )
+          ?.innerHTML.replaceAll(".", "") ?? null;
 
       return { title, image, price };
     });
 
     await browser.close();
 
-    if (
-      mercadoInfo.image == null ||
-      mercadoInfo.price == null ||
-      mercadoInfo.title == null
-    ) {
-      throw new Error("scrape error");
-    }
-
     res.status(200).json(mercadoInfo);
-  } catch (error) {
+  } catch (error: any) {
     console.log(error);
     res.status(500).json("scrape error");
   }
